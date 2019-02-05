5ef25e807b6d5347f4a773f6fbe54e97d8cf5d53
Silvio Henrique Ferreira <shferreira@me.com>
2019-02-05 02:46:35 -0200

    Add useMedia

---
+++ useMedia.js
@@ -0,0 +1,10 @@
+function useMedia(query) {
+  let [matches, setMatches] = useState(window.matchMedia(query).matches);
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  });
+  return matches;
+}
