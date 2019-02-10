8fb837005997666732ef66edd3800d7fad2497d4
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2019-02-10 01:32:10 -0200

    Create useStuck.js

---
+++ useStuck.js
@@ -0,0 +1,11 @@
+function useStuck(parent = ".wrapper") {
+  const ref = useRef();
+  useEffect(() => {
+    const wrapper = ref.current.closest(parent);
+    wrapper.addEventListener("scroll", () => {
+      ref.current.style.transform = `translate(0, ${wrapper.scrollTop}px)`;
+    });
+    () => wrapper.removeEventListener("scroll");
+  }, []);
+  return ref;
+}
