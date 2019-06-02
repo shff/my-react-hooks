8daa3ca21d84f88ca4ef550d8f516584d6a96ea6
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2019-06-02 14:26:40 -0300

    Fetch cancellation (#2)

--- useFetch.js
+++ useFetch.js
@@ -1,14 +1,14 @@
-//
-// Usage: const { response, data } = useFetch("/json.json");
-//
 import { useState, useEffect } from "react";
 
 export default function useFetch(url) {
   const [data, setData] = useState({ loading: true });
   useEffect(() => {
+    let active = true;
     fetch(url)
-      .then(response => response.json().then(data => setData({ response, data })))
+      .then(response => response.json())
+      .then(data => active && setData({ data }))
       .catch(error => setData({ error }));
+    return () => (active = false);
   }, [url]);
   return data;
-};
+}
